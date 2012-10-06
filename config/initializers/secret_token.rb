e792cfce2e6b82bf4ba9aa9ec0807a2cb45dd062
Vincent Leung <vincentleung1991@gmail.com>
2012-10-06 01:55:06 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Localbuzz::Application.config.secret_token = 'ab4a6d9077195e42b6139fea366c6b91f8af29d916e15b6ee53160e9057a9339cd04fc7081530ea0e184b2083a5e2e19dde3ca1e2b0215ac4655885bc9f31a78'
