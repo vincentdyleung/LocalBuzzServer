1294b081ebbaefb7958a7cc49e02e9111de49ab8
Vincent Leung <vincentleung1991@gmail.com>
2012-10-24 16:46:15 -0400

    validate uniqueness of fb_id, username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :fb_id, :first_name, :last_name, :password, :username
+  validates_uniqueness_of :fb_id, :username
   has_many :events
   has_many :users, :through => :join_events
 end
